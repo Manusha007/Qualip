1becf0cac65a9bbce69c261b0359eacd0ff488c6
lasaru2000 <lasaru.2019719@iit.ac.lk>
2021-03-16 01:47:49 +0530

    uploading backend code

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+hbs.registerPartials("");
+
+app.set();
+app.set());
+
+app.use(express.static("./public"));
+app.use('/', web);
+
+
+
+
+app.listen(1337, () => {
+	console.log('serving on port 1337');
+});
