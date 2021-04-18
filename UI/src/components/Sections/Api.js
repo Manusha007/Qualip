8c29ea5b1819ebde42be485f1d8ee0c59124741c
Deshan Vimukthi <vimudeshan123@gmail.com>
2021-04-19 00:49:02 +0530

    Edited about us page in UI

--- UI/src/components/Sections/Api.js
+++ UI/src/components/Sections/Api.js
@@ -8,10 +8,11 @@ function Api() {
      <ul className='cards__items'>
   
         <div class="column">
+        <h2>Manusha</h2>
                 <div class="show">
                     <img src="images/manusha.jpeg" alt="Manusha" />
                     <div class="view"><a href ="https://www.linkedin.com/in/manusha-pamoda/">
-                        <h2 class="name text">Manusha Pamoda</h2>
+                    <img src="images/manusha.jpeg" alt="Manusha" />
                     </a>
                     </div>
              
@@ -20,51 +21,61 @@ function Api() {
                  
         </div>
         <div class="column">
+            
+        <h2>Deshan</h2>
                 <div class="show">
                     <img src="images/deshan.jpg" alt="Deshan"/>
                     <div class="view"><a href ="https://www.linkedin.com/in/deshan-vimukthi/" >
-                        <h2 class="name text">Deshan Vimukthi</h2>
+                    <img src="images/deshan.jpg" alt="Deshan"/>
                     </a>
                     </div>
                 </div>
         </div>
         <div class="column">
+    
+            <h2>Dinujaya</h2>
+           
                 <div class="show">
                     <img src="images/Dinujaya.jpeg" alt="Dinujaya" />
                     <div class="view"><a href ="https://www.linkedin.com/in/dinujaya-munasinghe-0083061b8/">
-                        <h2 class="name text">Dinujaya Munasinghe</h2>
+                    <img src="images/Dinujaya.jpeg" alt="Dinujaya" />
                      </a>
                     </div>
                 </div>
         </div>
+      
         <div class="column">
+        <h2>Lasaru</h2>
                 <div class="show">
                     <img src="images/lasaru.jpeg" alt="Lasaru" />
                     <div class="view"><a href ="https://www.linkedin.com/in/lasaru-nilhan-4a34a61a6/">
-                        <h2 class="name text">Lasaru Nilhan</h2>
+                    <img src="images/lasaru.jpeg" alt="Lasaru" />
                     </a>
                     </div>
                 </div>
         </div>
         <div class="column">
+        <h2>Inura</h2>
                 <div class="show">
                     <img src="images/inura.jpeg" alt="Inura"/>
                     <div class="view"><a href ="https://www.linkedin.com/in/inura-dewage-6635a01b9" >
-                        <h2 class="name text">Inura Dewage</h2>
+                    <img src="images/inura.jpeg" alt="Inura"/>
                     </a>
                     </div>
                 </div>
         </div>
         <div class="column">
-                <div class="show">
+        <h2>Lahiru</h2>
+        <div class="show">
                     <img src="images/lahiru.jpg" alt="Lahiru"/>
                     <div class="view"><a href ="https://www.linkedin.com/in/lahiru-abhayakularathna-34797b1a5/" >
-                        <h2 class="name text">Lahiru Abhayakularathna</h2>
-                   
+                    <img src="images/lahiru.jpg" alt="Lahiru"/>
                     </a>
                     </div>
                 </div>
         </div>
+       
+      
         </ul>
     </div>
 
