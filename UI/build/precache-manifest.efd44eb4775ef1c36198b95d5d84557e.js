c68eec12982fd16c80820034f86493a5049f1f50
Lakshan.v <Lakshan.vithanage@eimsky.com>
2021-04-19 11:03:15 +0530

    initial commit

---
+++ UI/build/precache-manifest.efd44eb4775ef1c36198b95d5d84557e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3452302f7f009fbfec1dea57a245ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe27ee5eff3f749aa68",
+    "url": "/static/css/main.302c6066.chunk.css"
+  },
+  {
+    "revision": "2921a338bffcd63939d8",
+    "url": "/static/js/2.bda235f5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.bda235f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe27ee5eff3f749aa68",
+    "url": "/static/js/main.e3b382a5.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
