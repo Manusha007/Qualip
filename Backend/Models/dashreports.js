ff581cd9f8cd62637ea1bcaa6c9cad67467204f6
lasaru2000 <lasaru.2019719@iit.ac.lk>
2021-04-20 15:19:35 +0530

    add login routes

--- Backend/Models/dashreports.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const dashReportsSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  count: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('dashreports', dashReportsSchema);
